9780b324a2b87f4ce0639820c02df6e842e00881
Andre Morpa <andremorpa@gmail.com>
2020-05-29 14:40:59 +0100

    Change MongoURL

--- mongodb.ts
+++ mongodb.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
 const client = new MongoClient();
-client.connectWithUri("mongodb://localhost:27017");
+const MONGO_URL = "mongodb://localhost:27017";
+
+client.connectWithUri(MONGO_URL);
 
 const db = client.database("Deno");
 
